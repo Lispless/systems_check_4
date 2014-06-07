22041cd7cd50c8d361df849ea13d97222269954f
K <celfrost@hotmail.com>
2014-06-07 13:10:34 -0400

    Added skeleton fot box class

---
+++ lib/bounding_box.rb
@@ -0,0 +1,40 @@
+class BoundingBox
+  def initialize(left, bottom, width, height)
+    @left = left
+    @bottom = bottom
+    @width = width
+    @height = height
+  end
+
+  def width
+    @width
+  end
+
+  def height
+    @height
+  end
+
+  def left
+    @left
+  end
+
+  def right
+    @right = left + width
+  end
+
+  def top
+    @top = bottom + height
+  end
+
+  def bottom
+    @bottom
+  end
+
+  def contains_point?(x, y)
+    if x >= left && y >= bottom && x <= right && y <= height
+      true
+    else
+      false
+    end
+  end
+end
