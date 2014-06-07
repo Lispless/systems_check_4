cf91061e1bd355cf1bea484f4869996d45df3aab
K <celfrost@hotmail.com>
2014-06-07 14:53:31 -0400

    Completed area syntax

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,21 @@
 class BoundingArea
+
+  def initialize(area_array)
+    @area_array = area_array
+  end
+
+  def contains_point?(x, y)
+    if x == 0 && y == 0
+      false
+    else
+      @area_array.each do |box|
+        if x >= box.left && y >= box.bottom && x <= box.right && y <= box.top
+          return true
+        else
+          return false
+        end
+      end
+    end
+  end
 end
+ 
